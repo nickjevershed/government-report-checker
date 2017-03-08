b5a2ea582e0f7bd54693ca96cd28ada1cbe2b077
nickjevershed <nick.evershed@gmail.com>
2017-03-09 10:30:55 +1100

    updated to save docs

--- scraper.py
+++ scraper.py
@@ -1,40 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import requests
 import lxml.html
 import scraperwiki
 import random
 from Queue import Queue
+from datetime import datetime
 
+dateScraped = datetime.strftime(datetime.now(), '%Y-%m-%d')
 domain = "https://www.ag.gov.au"
-
+testing = False
+firstRun = True
 tovisit = Queue()
 visited = set()
 totalRequests = 0
 erroredRequests = 0
+files = ['pdf','doc','docx','xls','xlxs','epub','rtf','txt','ppt','pptx','odt']
+
+
+if testing:
+	print "Testing mode, data won't be saved"
+
+if firstRun:
+	print "First run, generating initial database of documents"
+
+
+
+
+def checkDocType(url):
+	extension = url.split(".")[-1]
+	if extension.lower() in files:
+		return True
+	else:
+		return False	
 
 def scrapePage(url):
-	global visited, tovisit, totalRequests, errorRequests
+	global visited, tovisit, totalRequests, erroredRequests
 	if url not in visited:
 		print "getting",url
 		visited.add(url)
 		totalRequests+=1
+
 		try:
-			r = requests.get(url)	
-			dom = lxml.html.fromstring(r.content)
-			linksOnPage = dom.cssselect("a")
-			internalLinks = []
+			
+			# Check if it's a doc, save it 
 
-			for link in linksOnPage:
-				if 'href' in link.attrib:
-					href = link.attrib['href']
+			if checkDocType(url):
+				r = requests.head(url)
+				lastModified = r.headers['last-modified']
+				data = {}
+				data['fileName'] = url.split("/")[-1]
+				data['url'] = url
+				data['lastModified'] = lastModified
+				data['dateScraped'] = dateScraped
+
+				if firstRun == True:
+					if not testing:
+						scraperwiki.sqlite.save(unique_keys=["url","lastModified"], data=data, table_name="allDocuments")
+
+				elif firstRun == False:
+					queryString = "* from allDocuments where url={url} and lastModified={lastModified}".format(url=url,lastModified=lastModified)
+					queryResult = scraperwiki.sqlite.select(queryString)
+
+					# if it hasn't been scraped before, save the data in the main table and the new docs table
+
+					if not queryResult:
+						print "new data, saving"
+						if not testing:
+							scraperwiki.sqlite.save(unique_keys=["url","lastModified"], data=data, table_name="allDocuments")
+							scraperwiki.sqlite.save(unique_keys=["url","lastModified","dateScraped"], data=data, table_name="newDocuments")
+
+					# if it has been saved before, check if it has been updated
 					
-					if href.startswith('http') and domain in href:
-						internalLinks.append(href)
+					else:
+						if data['lastModified'] != queryResult[0]['lastModified']:
 
-					elif href.startswith('/'):	
-						internalLinks.append(domain + href)
+							# it has been updated, so save the new values in the main database table and the updates table
 
-			for link in set(internalLinks):
-				tovisit.put(link)
+							print data['url'], "has been updated"
+							if not testing:
+								scraperwiki.sqlite.save(unique_keys=["url","lastModified"], data=data, table_name="allDocuments")
+								scraperwiki.sqlite.save(unique_keys=["url","lastModified","dateScraped"], data=data, table_name="updatedDocuments")
+
+			# If not a doc, get the page and scrape the links into the queue 	
+
+			else:	
+				r = requests.get(url)	
+				dom = lxml.html.fromstring(r.content)
+				linksOnPage = dom.cssselect("a")
+				internalLinks = []
+
+				for link in linksOnPage:
+					if 'href' in link.attrib:
+						href = link.attrib['href']
+						
+						if href.startswith('http') and domain in href:
+							internalLinks.append(href)
+
+						elif href.startswith('/'):	
+							internalLinks.append(domain + href)
+
+				for link in set(internalLinks):
+					tovisit.put(link)
 
 		except requests.exceptions.RequestException as e:
 			print e
@@ -42,10 +110,13 @@ def scrapePage(url):
 			if erroredRequests > 5:
 				print "their website is probs down, hey"
 
+# Start with the homepage
+
 tovisit.put(domain)
 
 while not tovisit.empty() and erroredRequests <= 5:
 	scrapePage(tovisit.get())
 	tovisit.task_done()
 
-# scraperwiki.sqlite.save(unique_keys=["blah"], data=data)
+# some sort of email notifications to go here
+
